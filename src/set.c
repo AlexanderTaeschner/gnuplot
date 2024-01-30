AlexanderTaeschner/gnuplot

commit 95ee13e2f8dd2657fe4ac6df45dea932701af849

default fillstyle for circle/polygon/ellipse objects is now "fs empty border"
If you set a linecolor for an empty circle/polygon/ellipse this has the
effect of changing the border property.  I.e.
	set obj 1 fs empty
	set obj 1 lc "red"
is now equivalent to
	set obj 1 fs empty border lc "red"

This is a CHANGE.  Previously the default was "fs solid border -1 fc bgnd"
for circles and ellipses, "fc 'black'" for polygons.

Rectangles remain a separate case and are unchanged by this commit.
Rectangles have always been treated differently than other object types.

src/set.c
@@ -4348,6 +4348,9 @@ set_obj(int tag, int obj_type)
 	if (!got_lt) {
 	    lp_style_type lptmp = this_object->lp_properties;
 	    lp_parse(&lptmp, LP_NOFILL, FALSE);
+	    if (this_object->fillstyle.fillstyle == FS_EMPTY) {
+		this_object->fillstyle.border_color = lptmp.pm3d_color;
+	    }
 	    if (c_token != save_token) {
 		this_object->lp_properties.l_width = lptmp.l_width;
 		this_object->lp_properties.d_type = lptmp.d_type;

demo/armillary.dem
@@ -49,7 +49,7 @@ array Longitude[13] = [267., 300., 328., 352., 29., 53., 90., 118., 138., 174., 
 do for [i=1:13] {
     set label i Zodiac[i] at polar Longitude[i], 1.1, 0 center font ",15"
     set obj i circle at polar Longitude[i], 1.1, 0 radius char 1.5
-    set obj i fs solid fc rgb hsv2rgb( Longitude[i]/360., 0.5, 1.0)
+    set obj i fs solid border lt -1 fc rgb hsv2rgb( Longitude[i]/360., 0.5, 1.0)
 }
 
 #

docs/gnuplot.doc
@@ -13484,11 +13484,11 @@ Ffigure_multiple_keys
  subsequent plots. You may define as many objects as you like. Currently the
  supported object types are `rectangle`, `circle`, `ellipse`, and `polygon`.
  Rectangles inherit a default set of style properties (fill, color, border) from
- those set by the command `set style rectangle`, but each object can also be
- given individual style properties. Circles, ellipses, and polygons  inherit the
- fill style from `set style fill`.  Objects to be drawn in 2D plots may be
- defined in any combination of axis, graph, polar, or screen coordinates.
+ those set by the command `set style rectangle`. Every object can be given
+ individual style properties when it is defined or in a later command.
 
+ Objects to be drawn in 2D plots may be defined in any combination of
+ axis, graph, polar, or screen coordinates.
  Object specifications in 3D plots cannot use graph coordinates.
  Rectangles and ellipses in 3D plots are limited to screen coordinates.
 

src/gadgets.h
@@ -611,18 +611,18 @@ extern struct object default_rectangle;
 
 extern struct object default_circle;
 #define DEFAULT_CIRCLE_STYLE { NULL, -1, 0, OBJ_CIRCLE, OBJ_CLIP, \
-	{FS_SOLID, 100, 0, BLACK_COLORSPEC},   			\
-	{0, LT_BACKGROUND, 0, DASHTYPE_SOLID, 0, 0, 1.0, 0.0, DEFAULT_P_CHAR, BACKGROUND_COLORSPEC, DEFAULT_DASHPATTERN}, \
+	{FS_EMPTY, 100, 0, {TC_DEFAULT, -2, 0}},   			\
+	{0, LT_BACKGROUND, 0, DASHTYPE_SOLID, 0, 0, 1.0, 0.0, DEFAULT_P_CHAR, BLACK_COLORSPEC, DEFAULT_DASHPATTERN}, \
 	{.circle = {1, {0,0,0,0.,0.,0.}, {graph,0,0,0.02,0.,0.}, 0., 360., TRUE }} }
 
 extern struct object default_ellipse;
 #define DEFAULT_ELLIPSE_STYLE { NULL, -1, 0, OBJ_ELLIPSE, OBJ_CLIP, \
-	{FS_SOLID, 100, 0, BLACK_COLORSPEC},   			\
-	{0, LT_BACKGROUND, 0, DASHTYPE_SOLID, 0, 0, 1.0, 0.0, DEFAULT_P_CHAR, BACKGROUND_COLORSPEC, DEFAULT_DASHPATTERN}, \
+	{FS_EMPTY, 100, 0, {TC_DEFAULT, -2, 0}},   			\
+	{0, LT_BACKGROUND, 0, DASHTYPE_SOLID, 0, 0, 1.0, 0.0, DEFAULT_P_CHAR, BLACK_COLORSPEC, DEFAULT_DASHPATTERN}, \
 	{.ellipse = {ELLIPSEAXES_XY, {0,0,0,0.,0.,0.}, {graph,graph,0,0.05,0.03,0.}, 0. }} }
 
 #define DEFAULT_POLYGON_STYLE { NULL, -1, 0, OBJ_POLYGON, OBJ_CLIP, \
-	{FS_SOLID, 100, 0, BLACK_COLORSPEC},   			\
+	{FS_EMPTY, 100, 0, {TC_DEFAULT, -2, 0}},   			\
 	{0, LT_BLACK, 0, DASHTYPE_SOLID, 0, 0, 1.0, 0.0, DEFAULT_P_CHAR, BLACK_COLORSPEC, DEFAULT_DASHPATTERN}, \
 	{.polygon = {0, NULL} } }
 

commit 6ada36b8bf4e6be5fdc6495d323220304b733e84

Regression: Border color of objects with fillstyle "empty"
Commit 95ee13e2f broke loading the border color of an empty object
as part of a single command
	set obj N fillstyle empty border <colorspec>
while adding the possibility of setting it in a separate command
	set obj N fillstyle empty
	set obj N lc <colorspec>

New unit test fs_empty.dem now checks that both forms are working.

Bug #2686

demo/all.dem
@@ -20,6 +20,7 @@ reset
 
 print "******************** file fillstyle.dem ********************"
 load "fillstyle.dem"
+load "fs_empty.dem"
 reset
 
 print "******************** file fillcvrs.dem ********************"

demo/fs_empty.dem
@@ -1,0 +1,34 @@
+#
+# Unit test to verify that the border color of an object with
+# fillstyle "empty" can be set as a simple line property.
+#
+version = sprintf("gnuplot %.1f patchlevel %s", GPVAL_VERSION, GPVAL_PATCHLEVEL)
+set title "All borders should be drawn in color\n".version offset 0, -1
+
+set for [i=1:12] obj i circle front
+
+set obj 1 at 2,1 fs empty border rgb "blue" lw 2
+set obj 2 at 3,1 fs empty border rgb "green" lw 2
+set obj 3 at 4,1 fs empty border rgb "orange" lw 2
+
+set obj 4 at 2,0 fs solid border rgb "blue" lw 2 fc "gray75" 
+set obj 5 at 3,0 fs solid border rgb "green" lw 2 fc "gray75" 
+set obj 6 at 4,0 fs solid border rgb "orange" lw 2 fc "gray75" 
+
+set obj 10 at 2,-1 fs empty border lw 2 ; set obj 10 lc "blue"
+set obj 11 at 3,-1 fs empty border lw 2 ; set obj 11 lc "green"
+set obj 12 at 4,-1 fs empty border lw 2 ; set obj 12 lc "orange"
+
+set obj 7 at 2,-2 fs empty border lw 2 lc "black"; set obj 7 lc "blue"
+set obj 8 at 3,-2 fs empty border lw 2 lc "black"; set obj 8 lc "green"
+set obj 9 at 4,-2 fs empty border lw 2 lc "black"; set obj 9 lc "orange"
+
+set yrange [-3:2]
+set xrange [1:5]
+unset border
+unset tics
+plot x with lines lt nodraw notitle
+
+pause -1 "<cr> to continue"
+reset
+

src/set.c
@@ -4102,6 +4102,7 @@ set_obj(int tag, int obj_type)
     TBOOLEAN got_corners = FALSE;
     TBOOLEAN got_center = FALSE;
     TBOOLEAN got_origin = FALSE;
+    lp_style_type lptmp;
 
     c_token++;
 
@@ -4411,11 +4412,8 @@ set_obj(int tag, int obj_type)
 	/* Line properties (will be used for the object border if the fillstyle has one. */
 	/* LP_NOFILL means don't eat fillcolor here since at is set separately with "fc". */
 	if (!got_lt) {
-	    lp_style_type lptmp = this_object->lp_properties;
+	    lptmp = this_object->lp_properties;
 	    lp_parse(&lptmp, LP_NOFILL, FALSE);
-	    if (this_object->fillstyle.fillstyle == FS_EMPTY) {
-		this_object->fillstyle.border_color = lptmp.pm3d_color;
-	    }
 	    if (c_token != save_token) {
 		this_object->lp_properties.l_width = lptmp.l_width;
 		this_object->lp_properties.d_type = lptmp.d_type;
@@ -4426,6 +4424,11 @@ set_obj(int tag, int obj_type)
 	}
 
 	int_error(c_token, "Unrecognized or duplicate option");
+    }
+
+    /* This handles the case "set obj N circle fs empty; set obj N lc "red" */
+    if (got_lt && !got_fill && !got_fc && this_object->fillstyle.fillstyle == FS_EMPTY)  {
+	    this_object->fillstyle.border_color = lptmp.pm3d_color;
     }
 
     if (got_center && got_corners)