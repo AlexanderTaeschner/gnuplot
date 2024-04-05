c2e1c656720ad1b738bef1c2d99f3579f29afd37
Ethan A Merritt <merritt@u.washington.edu>
2024-04-05 14:37:10 -0700

    fix order-dependent parsing of 2D plots with "fillstyle solid fillcolor variable"
    Modify varcolor.dem to trigger the error if it is present.

--- src/plot2d.c
+++ src/plot2d.c
@@ -2935,6 +2935,10 @@ eval_plots()
 		    if (equals(c_token,"fc") || almost_equals(c_token,"fillc$olor")) {
 			parse_colorspec(&fillcolor, TC_VARIABLE);
 			set_fillcolor = TRUE;
+			if (fillcolor.type == TC_VARIABLE) {
+			    this_plot->lp_properties.l_type = LT_COLORFROMCOLUMN;
+			    set_lpstyle = TRUE;
+			}
 		    }
 		    if (stored_token != c_token)
 			continue;
