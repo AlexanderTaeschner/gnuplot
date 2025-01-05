128ef7421ef85ba17ad666c5185740c73d60bb90
Ethan A Merritt <merritt@u.washington.edu>
2025-01-05 13:57:27 -0800

    'plot with yerrorbars' Do not use error bar to determine "out of range"
    It is OK to use the error bar span for autoscaling on y, but it
    should not be used to determine whether the point itself is in-range.
    
    Bug 2749

--- src/plot2d.c
+++ src/plot2d.c
@@ -1615,10 +1615,13 @@ store2d_point(
     case YERRORLINES:		/* auto-scale ylow yhigh */
 	cp->xlow = xlow;	/* really theta if polar; CRD_PTSIZE otherwise */
 	cp->xhigh = xhigh;	/* really theta if polar; CRD_PTTYPE otherwise */
+	/* autoscale to ymin and ymax but do not mark point itself as outrange */
+	dummy_type = cp->type;
 	STORE_AND_UPDATE_RANGE(cp->ylow, ylow, cp->type, current_plot->y_axis,
 				current_plot->noautoscale, cp->ylow = -VERYLARGE);
 	STORE_AND_UPDATE_RANGE(cp->yhigh, yhigh, cp->type, current_plot->y_axis,
 				current_plot->noautoscale, cp->yhigh = -VERYLARGE);
+	cp->type = dummy_type;
 	break;
     case BOXES:			/* auto-scale to xlow xhigh */
     case BOXPLOT:		/* auto-scale to xlow xhigh, factor is already in z */
