30df9ca2c37f74ee6eaa0009ccfd7f692a40b29c
Ethan A Merritt <merritt@u.washington.edu>
2025-02-24 23:10:31 -0800

    "clear" command must use FS_EMPTY
    10 months ago commit 83e4eadb changed enum t_fillstyle to not include
    a value of 0.  This was an attempt to catch anywhere in the code that
    passed a bare value rather than an enum.  Finally it caught something,
    the call to term->fillbox() in clear_command.
    I expected more help from the compiler on this one.
    
    Bug #2767

--- src/command.c
+++ src/command.c
@@ -1227,7 +1227,7 @@ clear_command()
 	int yy1 = yoffset * term->ymax;
 	unsigned int width = xsize * term->xmax;
 	unsigned int height = ysize * term->ymax;
-	(*term->fillbox) (0, xx1, yy1, width, height);
+	(*term->fillbox)(FS_EMPTY, xx1, yy1, width, height);
     }
     term_end_plot();
 
