AlexanderTaeschner/gnuplot

commit fc98188b6f484cfaeba007bb4cb7669cbf4009bf

3D polygons from "splot with polygons" use per-plot border color and linewidth
Previously all 3D polygons shared the border properties from "set pm3d".
This limitation has been documented as an implementation glitch to be
removed at some point.  Now it is relaxed for polygons from "splot with polygons".
The border linewidth and color are taken from the splot command line, if present,
otherwise from "set style fill".

Modify delaunay demo to specify fillcolor in splot command
Modify polygons demo to specify linewidth in splot command

Notes:

- I do not understand the visible change to lines drawn by the polygon whale demo

src/misc.c
@@ -1072,10 +1072,8 @@ lp_parse(struct lp_style_type *lp, lp_class destination_class, TBOOLEAN allow_po
 	 * fc colorspec as a line property.  We need to parse it later as a
 	 * _fill_ property. Also prevents "plot ... fc <col1> fs <foo> lw <baz>"
 	 * from generating an error claiming redundant line properties.
-	 * FIXME:  Why exclude "fc palette"??
 	 */
 	if ((destination_class == LP_NOFILL || destination_class == LP_ADHOC)
-	&&  (!almost_equals(c_token+1, "pal$ette"))
 	&&  (equals(c_token,"fc") || almost_equals(c_token,"fillc$olor"))) {
 	    FPRINTF((stderr, "ignoring 'fc' request\n"));
 	    break;

demo/delaunay.dem
@@ -8,7 +8,8 @@
 if (!strstrt(GPVAL_COMPILE_OPTIONS, "+CHI_SHAPES")) {
     print "Your copy of gnuplot was not built with support for chi shapes"
 } else {
-    set pm3d border lc "black" lw 0.5
+    set style fill solid border lc "black"
+    unset colorbox
     set palette viridis
     set xyplane 0
     set isotropic
@@ -21,10 +22,10 @@ if (!strstrt(GPVAL_COMPILE_OPTIONS, "+CHI_SHAPES")) {
     set multiplot layout 1,2
 
     set pm3d lighting primary 0.5 spec 0.2 spec2 0.3
-    splot 'hemisphr.dat' using 1:2:3 delaunay with polygons title "pm3d lighting"
+    splot 'hemisphr.dat' using 1:2:3 delaunay with polygons fc palette z title "pm3d lighting"
 
     set pm3d nolighting
-    splot 'hemisphr.dat' using 1:2:3 delaunay with polygons title "no lighting model"
+    splot 'hemisphr.dat' using 1:2:3 delaunay with polygons fc palette z title "no lighting model"
 
     unset multiplot
 }

demo/polygons.dem
@@ -71,6 +71,7 @@ set xrange [-2:2]; set yrange [-2:2]; set zrange [-2:2]
 set view equal xyz
 set view 30,30,1.5
 unset border
+unset colorbox
 unset tics
 unset key
 unset label
@@ -94,23 +95,23 @@ set pm3d lighting spec2 0.6
 
 set title "splot icosahedron.dat with polygons"
 
-splot 'icosahedron.dat' with polygons fc "gray75"
+splot 'icosahedron.dat' with polygons fc "gray75" lw 2
 
 pause -1 "Hit return to continue"
 
-set title "splot dodecahedron.dat with polygons lc variable"
+set title "splot dodecahedron.dat with polygons fc variable"
 set view 148, 55, 2.7
 set pm3d lighting spec2 0.3
 set palette cubehelix
 set for [i=1:12] linetype 99+i lc palette frac i/12.
 
-splot 'dodecahedron.dat' using 1:2:3:(column(-1)+3) with polygons lc variable
+splot 'dodecahedron.dat' using 1:2:3:(column(-1)+3) with polygons fc variable lw 2
 
 pause -1 "Hit return to continue"
 
 set title "splot dodecahedron with polygons fc rgb variable"
 
-splot 'dodecahedron.dat' using 1:2:3:(0xffffff * rand(0)) with polygons fc rgb variable
+splot 'dodecahedron.dat' using 1:2:3:(0xffffff * rand(0)) with polygons fc rgb variable lw 2
 
 pause -1 "Hit return to continue"
 
@@ -119,7 +120,7 @@ set view 36, 60, 4.2
 set style fill transparent solid 0.75
 set pm3d nolighting border lt -1 lw 2
 
-splot 'truncated_cube.dat' using ($1-0.5):($2-0.5):($3-0.5) with polygons fc bgnd
+splot 'truncated_cube.dat' using ($1-0.5):($2-0.5):($3-0.5) with polygons fc bgnd lw 2
 
 pause -1 "Hit return to continue"
 

docs/gnuplot.doc
@@ -655,9 +655,11 @@ D epi_data 1
 #b Blank line equivalent for binary data files, needed to support binary
 ## input for plot styles that expect a blank line to separate data items.
 ## See `binary blank`.
-#b In previous gnuplot versions all 3D polygons shared a single border color
-## and linewidth taken from "set pm3d".  Now individual 3D polygon objects take
-## their fill border and line width from "set object".
+#b In previous gnuplot versions all 3D polygons, objects, and filled areas
+## shared a single border color and linewidth taken from "set pm3d".
+## This limitation is now removed; border properties can be specified per-plot
+## or per-object. This change affects any scripts that expected "set pm3d" to
+## affect the borders of 3D polygons and boxes.
 #b splot with contourfill at base
 #b Revised implementation of local variables (faster, more well-defined scope).
 #b "linestyle variable"
@@ -6240,11 +6242,14 @@ Ffigure_points
  input file.  A blank line separates one polygon from the next. For the case
  of binary input files, see `binary blank`.
 
- The fill style and color may be specified in the splot command, otherwise the
- global fillstyle from `set style fill` is used.  Due to limitations in the
- pm3d code, a single border line style from `set pm3d border` is applied to all
- polygons from `splot with polygons`. This restriction may be relaxed in a later
- gnuplot version, that more strongly distinguishes line color from fill color.
+ Note: Previous gnuplot versions suffered from a limitation that all 3D polygons
+ shared a single set of fill border proporties taken from `set pm3d border`.
+ This limitation is no longer present.
+ Polygon fill style, border color, and border linewidth may now be specified in
+ the splot command, otherwise they are taken from the global settings of
+ `set style fill` (not from `set pm3d`).
+ The fill style, border color, and border linewidth for polygon objects are
+ specified in the `set object` command.
 
  Each polygon may be assigned a separate fill color by providing a fourth using
  specifier and the keywords `fc variable` (value is interpreted as a linetype)
@@ -6260,9 +6265,8 @@ Ffigure_polygons
       unset border
       unset tics
       set pm3d depth
-      set pm3d border lc "black" lw 1.5
-      splot 'icosahedron.dat' with polygons \
-            fs transparent solid 0.8 fc bgnd
+      splot 'icosahedron.dat' with polygons fc bgnd \
+            fs transparent solid 0.8 border lc "black" lw 2
 
 2 rgbalpha
 ?plotting styles rgbalpha

src/graph3d.c
@@ -4229,14 +4229,15 @@ plot3d_polygons(struct surface_points *plot)
 	if (nv < 3)
 	    continue;
 
-	/* Coloring piggybacks on options for isosurface */
+	/* Coloring taken from fillstyle (which confusingly is in plot->lp_properties) */
 	if (plot->pm3d_color_from_column && !isnan(points[0].CRD_COLOR))
 	    quad[0].c = points[0].CRD_COLOR;
-	else if (plot->fill_properties.border_color.type == TC_DEFAULT) {
+	else if (plot->lp_properties.pm3d_color.type == TC_Z
+	     ||  plot->lp_properties.pm3d_color.type == TC_DEFAULT) {
 	    double z = pm3d_assign_triangle_z(points[0].z, points[1].z, points[2].z);
 	    quad[0].c = rgb_from_gray(cb2gray(z));
 	} else
-	    quad[0].c = plot->fill_properties.border_color.lt;
+	    quad[0].c = plot->lp_properties.pm3d_color.lt;
 	quad[1].c = style;
 	pm3d_add_polygon( plot, quad, nv );
     }

src/plot3d.c
@@ -1272,6 +1272,13 @@ get_3ddata(struct surface_points *this_plot)
 			color = v[3];
 			color_from_column(TRUE);
 		    }
+		    /* This handles "with polygons fc variable" */
+		    if (this_plot->lp_properties.pm3d_color.type == TC_VARIABLE) {
+			struct lp_style_type lptmp;
+			load_linetype(&lptmp, (int)(v[3]));
+			color = lptmp.pm3d_color.lt;
+			color_from_column(TRUE);
+		    }
 		    /* This handles "with polygons fc rgb variable" */
 		    if (this_plot->fill_properties.border_color.type == TC_RGB
 		    &&  this_plot->fill_properties.border_color.value < 0) {
@@ -1688,6 +1695,7 @@ eval_3dplots()
 	    TBOOLEAN set_labelstyle = FALSE;
 	    TBOOLEAN set_fillstyle = FALSE;
 	    TBOOLEAN set_fillcolor = FALSE;
+	    TBOOLEAN set_lc = FALSE;
 	    t_colorspec fillcolor = DEFAULT_COLORSPEC;
 	    TBOOLEAN set_smooth = FALSE;
 
@@ -2113,6 +2121,7 @@ eval_3dplots()
 		if (this_plot->plot_style != LABELPOINTS) {
 		    int stored_token = c_token;
 		    struct lp_style_type lp = DEFAULT_LP_STYLE_TYPE;
+		    t_colorspec starting_lc;
 		    int new_lt = 0;
 
 		    lp.l_type = line_num;
@@ -2120,12 +2129,15 @@ eval_3dplots()
 		    lp.d_type = line_num;
 
 		    load_linetype(&lp, line_num+1);
+		    starting_lc = lp.pm3d_color;
 
  		    new_lt = lp_parse(&lp, LP_ADHOC,
 			     this_plot->plot_style & PLOT_STYLE_HAS_POINT);
 
 		    checked_once = TRUE;
 		    if (stored_token != c_token) {
+			if (memcmp(&starting_lc, &lp.pm3d_color, sizeof(t_colorspec)))
+			    set_lc = TRUE;
 			if (set_lpstyle) {
 			    duplication=TRUE;
 			    break;
@@ -2176,41 +2188,41 @@ eval_3dplots()
 		}
 
 		/* Some plots have a fill style as well */
-		if ((this_plot->plot_style & PLOT_STYLE_HAS_FILL) && !set_fillstyle){
-		    int stored_token = c_token;
+		if (!set_fillstyle) {
+		    if ((this_plot->plot_style & PLOT_STYLE_HAS_FILL)
+		    ||  (pm3d.implicit == PM3D_IMPLICIT)) {
+			int stored_token = c_token;
 
-		    if (this_plot->plot_style == CONTOURFILL) {
-			this_plot->fill_properties.fillstyle = FS_SOLID;
-			this_plot->fill_properties.filldensity = 100;
-			if (!set_lpstyle) {
-			    /* TC_DEFAULT indicates "retrace", TC_LT would give "noborder" */
-			    this_plot->fill_properties.border_color.type = TC_DEFAULT;
-			    this_plot->fill_properties.border_color.lt = LT_NODRAW;
-			}
-		    } else {
-			this_plot->fill_properties.fillstyle = default_fillstyle.fillstyle;
-			this_plot->fill_properties.filldensity = default_fillstyle.filldensity;
-			this_plot->fill_properties.fillpattern = 1;
-			if (this_plot->fill_properties.fillstyle == FS_EMPTY)
+			if (this_plot->plot_style == CONTOURFILL) {
 			    this_plot->fill_properties.fillstyle = FS_SOLID;
+			    this_plot->fill_properties.filldensity = 100;
+			    if (!set_lpstyle) {
+				/* TC_DEFAULT indicates "retrace", TC_LT would give "noborder" */
+				this_plot->fill_properties.border_color.type = TC_DEFAULT;
+				this_plot->fill_properties.border_color.lt = LT_NODRAW;
+			    }
+			} else {
+			    this_plot->fill_properties.fillstyle = default_fillstyle.fillstyle;
+			    this_plot->fill_properties.filldensity = default_fillstyle.filldensity;
+			    this_plot->fill_properties.fillpattern = 1;
+			    if (this_plot->fill_properties.fillstyle == FS_EMPTY)
+				this_plot->fill_properties.fillstyle = FS_SOLID;
+			}
+			if (equals(c_token,"fs") || almost_equals(c_token,"fill$style")) {
+			    parse_fillstyle(&this_plot->fill_properties);
+			    set_fillstyle = TRUE;
+			}
+			if (this_plot->plot_style == PM3DSURFACE)
+			    this_plot->fill_properties.border_color.type = TC_DEFAULT;
+			if (equals(c_token,"fc") || almost_equals(c_token,"fillc$olor")) {
+			    parse_colorspec(&fillcolor, TC_VARIABLE);
+			    if (fillcolor.type == TC_COLORMAP)
+				this_plot->lp_properties.colormap = get_colormap(c_token++);
+			    set_fillcolor = TRUE;
+			}
+			if (stored_token != c_token)
+			    continue;
 		    }
-		    if (equals(c_token,"fs") || almost_equals(c_token,"fill$style")) {
-			parse_fillstyle(&this_plot->fill_properties);
-			set_fillstyle = TRUE;
-		    }
-		    if (this_plot->plot_style == PM3DSURFACE)
-			this_plot->fill_properties.border_color.type = TC_DEFAULT;
-		    if (equals(c_token,"fc") || almost_equals(c_token,"fillc$olor")) {
-			parse_colorspec(&fillcolor, TC_RGB);
-			/* FIXME: can't happen because we limited the options to TC_RGB
-			if (fillcolor.type == TC_COLORMAP)
-			    this_plot->lp_properties.colormap = get_colormap(c_token++);
-			 */
-			set_fillstyle = TRUE;
-			set_fillcolor = TRUE;
-		    }
-		    if (stored_token != c_token)
-			continue;
 		}
 
 		/* EXPERIMENTAL filter splot ... if (<expression>) */
@@ -2307,19 +2319,27 @@ eval_3dplots()
 
 	    /* If this plot style uses a fillstyle and we saw an explicit
 	     * fill color, save it in lp_properties now.
-	     * FIXME: make other plot styles work like BOXES.
-	     *        ZERRORFILL and CONTOURFILL are weird.
+	     * FIXME: some styles still expect fillcolor to be in border_color.
 	     */
-	    if ((this_plot->plot_style & PLOT_STYLE_HAS_FILL) && set_fillcolor) {
-		if (this_plot->plot_style == ZERRORFILL
-		||  this_plot->plot_style == FILLEDCURVES
-		||  this_plot->plot_style == CONTOURFILL) {
-		    this_plot->fill_properties.border_color = this_plot->lp_properties.pm3d_color;
-		    this_plot->lp_properties.pm3d_color = fillcolor;
-		} else if (this_plot->plot_style == BOXES) {
-		    this_plot->lp_properties.pm3d_color = fillcolor;
-		} else {
-		    this_plot->fill_properties.border_color = fillcolor;
+	    if (set_fillcolor) {
+		if ((this_plot->plot_style & PLOT_STYLE_HAS_FILL)
+		||  (pm3d.implicit == PM3D_IMPLICIT)) {
+		    if (this_plot->plot_style == ZERRORFILL
+		    ||  this_plot->plot_style == FILLEDCURVES
+		    ||  this_plot->plot_style == CONTOURFILL) {
+			this_plot->fill_properties.border_color
+				    = this_plot->lp_properties.pm3d_color;
+			this_plot->lp_properties.pm3d_color = fillcolor;
+		    } else if (this_plot->plot_style == BOXES) {
+			this_plot->lp_properties.pm3d_color = fillcolor;
+		    } else if (this_plot->plot_style == POLYGONS) {
+			if (set_lc && !set_fillstyle)
+			    this_plot->fill_properties.border_color
+				    = this_plot->lp_properties.pm3d_color;
+			this_plot->lp_properties.pm3d_color = fillcolor;
+		    } else {
+			this_plot->fill_properties.border_color = fillcolor;
+		    }
 		}
 	    }
 

src/pm3d.c
@@ -1727,7 +1727,8 @@ filled_polygon(struct surface_points *from_plot, int index, gpdPoint *corners, i
 	    return;
 	if (bordercolor->type != TC_DEFAULT)
 	    apply_pm3dcolor(bordercolor);
-    } else if (from_plot &&  (from_plot->plot_style == BOXES)) {
+    } else if ((from_plot->plot_style == BOXES)
+	   ||  (from_plot->plot_style == POLYGONS)) {
 	t_colorspec *bordercolor = &(from_plot->fill_properties.border_color);
 	if (bordercolor->type == TC_LT && bordercolor->lt == LT_NODRAW)
 	    return;

commit 03217b7b9b398462f07163df14fdcc5cccdc4737

Fix regression in use of named palettes for "splot with pm3d"
The regression, introduced by commit fc98188b6, only manifests when a
named palette is used for splot "with pm3d fc palette" and no line properties
precede the fillcolor in the splot command. In this circumstance the
TC_COLORMAP type and colormap are correctly read but stored as a
line property rather than a fill color.

This commit reverts deletion of a test in lp_parse() that would prevent
this error.

named_palettes.dem is modified so that the regression and fix are
clearly visible when run as a unit test.

Bug 2809

demo/named_palettes.dem
@@ -49,10 +49,12 @@ pause -1 "<cr> to continue"
 # Method 2 (new in version 6)
 #	"set pm3d interpolate" works now
 #	"set pm3d lighting" also works
-set title "Version 6 offers a new keyword to access this palette"
+set title "Version 6 offers a new keyword to access this palette\n(should look the same)"
+set title offset 0,1
 
 set palette defined (0 "dark-blue", 1 "white")
 set colormap new bluemap
+set palette defined (0 "black", 1 "black")
 splot '++' using 1:2:(foo($1,$2)) with pm3d fc palette bluemap\
            title "pm3d using 1:2:3 with pm3d fc palette bluemap"
 

src/misc.c
@@ -1058,9 +1058,13 @@ lp_parse(struct lp_style_type *lp, lp_class destination_class, TBOOLEAN allow_po
 	 * fc colorspec as a line property.  We need to parse it later as a
 	 * _fill_ property. Also prevents "plot ... fc <col1> fs <foo> lw <baz>"
 	 * from generating an error claiming redundant line properties.
+	 * The test for "fillcolor palette" allows "splot with pm3d fc palette <colormap>"
+	 * to NOT store the palette request in this_plot->lp_properties and
+	 * instead catch it later in a subsequent search for fillcolor.
 	 */
 	if ((destination_class == LP_NOFILL || destination_class == LP_ADHOC)
-	&&  (equals(c_token,"fc") || almost_equals(c_token,"fillc$olor"))) {
+	&&  (equals(c_token,"fc") || almost_equals(c_token,"fillc$olor"))
+	&&  (!almost_equals(c_token+1, "pal$ette"))) {
 	    FPRINTF((stderr, "ignoring 'fc' request\n"));
 	    break;
 	}